e1dc1fdff99b0e5ea4d35976b3c7b9907d4e0e00
tkeiyama <thomas18thx@gmail.com>
2026-02-17 12:01:04 +0000

    split format

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-  "*.{js,ts,tsx}": ["pnpm lint:fix", "pnpm format"],
+  "*.{js,ts,tsx}": ["pnpm lint:fix", "pnpm format:fix"],
   "*.{ts,tsx}": () => "pnpm typecheck",
-  "*.{json,yaml,html,css,md}": ["pnpm format"],
+  "*.{json,yaml,html,css,md}": ["pnpm format:fix"],
 };
