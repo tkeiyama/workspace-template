1015ed51da4b8f9c141dcd1f816ad925e745f9cd
tkeiyama <thomas18thx@gmail.com>
2026-02-17 11:59:47 +0000

    split lint

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-  "*.{js,ts,tsx}": ["pnpm lint", "pnpm format"],
+  "*.{js,ts,tsx}": ["pnpm lint:fix", "pnpm format"],
   "*.{ts,tsx}": () => "pnpm typecheck",
   "*.{json,yaml,html,css,md}": ["pnpm format"],
 };
